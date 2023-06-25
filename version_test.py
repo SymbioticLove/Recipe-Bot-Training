f1bb38909e7b98456385a71a7b696f45fb158528
Symbiotic Love <matthew@symbiotic.love>
2023-06-25 14:22:20 -0700

    Delete version_test.py

--- version_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import sklearn
-import tensorflow as tf
-
-print("NumPy version:", np.__version__)
-print("Pandas version:", pd.__version__)
-print("scikit-learn version:", sklearn.__version__)
-print("TensorFlow version:", tf.__version__)
